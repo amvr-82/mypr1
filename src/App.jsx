d42b53c129816caed102743d475bb357971323cb
amirhossein <amirhossein.develope@gmail.com>
2025-11-01 17:42:07 +0330

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -19,4 +19,3 @@ function Navbar() {
 }
 
 export default App;
-export default Navbar;
