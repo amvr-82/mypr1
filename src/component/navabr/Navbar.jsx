4cb822ba8d58edc9cf025f467ac5f5bfafd9e2c8
amirhossein <amirhossein.develope@gmail.com>
2025-11-02 10:57:40 +0330

    سی

--- src/component/navabr/Navbar.jsx
+++ src/component/navabr/Navbar.jsx
@@ -1,15 +1,23 @@
 function Navbar() {
   return (
-    <ul
-      className="bg-white shadow-2xl flex justify-start p-4 gap-4
+    <div>
+      <h1
+        className="bg-white shadow-2xl flex justify-start p-4 gap-4
      fixed top-0  w-full flex-row-reverse  "
-    >
-      <li>فهرست</li>
-      <li>خدمات</li>
-      <li>بلاک</li>
-      <li>تماس با ما</li>
-      <li>درباره ما</li>
-    </ul>
+      >
+        ریکت
+      </h1>
+      <ul
+        className="bg-white shadow-2xl flex justify-start p-4 gap-4
+     fixed top-0  w-full flex-row-reverse  "
+      >
+        <li>فهرست</li>
+        <li>خدمات</li>
+        <li>بلاک</li>
+        <li>تماس با ما</li>
+        <li>درباره ما</li>
+      </ul>
+    </div>
   );
 }
 
