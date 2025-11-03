c7d4e7fd993a03f98976708a5091f0c44d527edc
amirhossein <amirhossein.develope@gmail.com>
2025-11-03 10:54:28 +0330

    sd

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,57 +1,26 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./component/Navbar/Navbar";
 import Article from "./component/Article/Article";
-import axios, { isCancel, AxiosError } from "axios";
-
-console.log(axios.isCancel("something"));
+import axios from "axios";
 
 function Home() {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    setArticles([
-      {
-        title: "دیتا اول",
-        author: "من ",
-        date: "Nov 1, 2025",
-        summary: "خلاصه 1",
-        src: "./src/assets/aks.avif",
-      },
-      {
-        title: "دیتا اول",
-        author: "من ",
-        date: "Nov 1, 2025",
-        summary: "خلاصه 1",
-        src: "./src/assets/aks.avif",
-      },
-      {
-        title: " دیتا دوم",
-        author: "سامان",
-        date: "Nov 2, 2025",
-        summary: "خلاصه دوم",
-        src: "./src/assets/aks.avif",
-      },
-      {
-        title: "دیتا اول",
-        author: "من ",
-        date: "Nov 1, 2025",
-        summary: "خلاصه 1",
-        src: "./src/assets/aks.avif",
-      },
-      {
-        title: " دیتا دوم",
-        author: "سامان",
-        date: "Nov 2, 2025",
-        summary: "خلاصه دوم",
-        src: "./src/assets/aks.avif",
-      },
-    ]);
+    axios
+      .get("http://localhost:8000/articles")
+      .then((res) => {
+        console.log(res.data);
+        setArticles(res.data);
+      })
+      .catch((err) => {
+        console.error("خطا در گرفتن مقاله‌ها:", err);
+      });
   }, []);
 
   return (
     <div>
       <Navbar />
-
       <div>
         <h2 className="mt-[85px] p-5 flex justify-end font-bold text-[25px]">
           سلام بر مقاله
@@ -59,9 +28,10 @@ function Home() {
       </div>
 
       <div className="grid grid-cols-4 items-end gap-4 p-4">
-        {articles.map((article, index) => (
-          <Article key={index} {...article} />
-        ))}
+        {Array.isArray(articles) &&
+          articles.map((article, index) => (
+            <Article key={index} {...article} />
+          ))}
       </div>
     </div>
   );
